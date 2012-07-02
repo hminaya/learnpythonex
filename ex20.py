ca7ba0ba5f9d45e5715feab9dc42f037ca94fdc3
Hector Minaya <hminaya@gmail.com>
2012-07-02 00:40:01 -0400

    done for the night

--- ex20.py
+++ ex20.py
@@ -12,3 +12,22 @@ def print_a_line(line_count,f):
 	print line_count, f.readline()
 
 current_file = open(input_file)
+
+print "First let's print the whole file:\n"
+
+print_all(current_file)
+
+print "Now let's rewind"
+
+rewind(current_file)
+
+print "Just three lines:"
+
+current_line = 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
